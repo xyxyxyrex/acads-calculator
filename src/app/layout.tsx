9c2c74da86612c4cd3b8e9fe1506fc91ce2f214e
Xyrex Kyle Salazar <xyrexkyle@gmail.com>
2025-06-15 06:48:04 +0800

    nah

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,10 +25,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <Providers>
-          {children}
-        </Providers>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,14 +1,12 @@
 import { pgTable, text, serial, timestamp } from "drizzle-orm/pg-core";
 
-
 export const users = pgTable("users", {
-    id: serial("id").primaryKey(),
-    name: text("name").notNull(),
-    email: text("email").notNull().unique(),
-    password: text("password").notNull(),
-    createdAt: timestamp("created_at").notNull().defaultNow(),
-    updatedAt: timestamp('updated_at').defaultNow().$onUpdateFn(() => new Date())
+  id: serial("id").primaryKey(),
+  name: text("name").notNull(),
+  email: text("email").notNull().unique(),
+  password: text("password").notNull(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at")
+    .defaultNow()
+    .$onUpdateFn(() => new Date()),
 });
-
-
-
